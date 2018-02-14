40e02d4df018004718ef86bc788689d0a2bb0514
Duane Johnson <mr.duane.johnson@gmail.com>
2018-02-13 20:38:41 -0800

    Add files via upload
    Added additional modules, and adjusted the workspace entries for recon-ng. Added one report format to reduce clutter and make it easier to load the files into python for additional recon

--- enumall.py
+++ enumall.py
@@ -10,19 +10,23 @@
 # Alt-DNS Download: https://github.com/infosec-au/altdns
 #
 # by @jhaddix and @leifdreizler
+# Modified by Th3R0ck
 
 import argparse
 import re
-import sys
 import datetime
 import time
-import os
+import os, random
 import sys
+import math
+
 try:
 	from config import *
 except:
 	reconPath = "/usr/share/recon-ng/"
-	altDnsPath = "/root/Desktop/altdns-master/"
+	altDnsPath = "/opt/altdns/"
+
+
 
 sys.path.insert(0,reconPath)
 from recon.core import base
@@ -31,45 +35,84 @@ from recon.core.framework import Colors
 if altDnsPath:
 	sys.path.insert(1, altDnsPath)
 
+RESOLVERS_FILE = "/opt/massdns/usresolvers.txt"
 
 def run_module(reconBase, module, domain):
-	x = reconBase.do_load(module)
-	x.do_set("SOURCE " + domain)
-	x.do_run(None)
+    x = reconBase.do_load(module)
+    x.do_set("SOURCE " + domain)
+    x.do_run(None)
 
+def load_resolvers(infile):
+    resolvers_list = list()
+    if os.path.isfile(infile):
+        with open(infile) as data:
+            for line in data:
+                line = line.rstrip()
+                resolvers_list.append(line)
+
+    return resolvers_list
+
+def get_random_resolver_ip(resolver_file=RESOLVERS_FILE):
+    resolvers_list = load_resolvers(resolver_file)
+
+    return resolvers_list[random.randint(0,len(resolvers_list)-1)]
 
 def run_recon(domains, bruteforce):
-	stamp = datetime.datetime.now().strftime('%M:%H-%m_%d_%Y')
-	wspace = domains[0]+stamp
+    #stamp = datetime.datetime.now().strftime('%M:%H-%m_%d_%Y')
+    #wspace = domains[0]+stamp
 
-	reconb = base.Recon(base.Mode.CLI)
-	reconb.init_workspace(wspace)
-	reconb.onecmd("TIMEOUT=100")
-	module_list = ["recon/domains-hosts/bing_domain_web", "recon/domains-hosts/google_site_web", "recon/domains-hosts/netcraft", "recon/domains-hosts/shodan_hostname", "recon/netblocks-companies/whois_orgs", "recon/hosts-hosts/resolve"]
-	
-	for domain in domains:
-		for module in module_list:
-				run_module(reconb, module, domain)
-	
-		#subdomain bruteforcing
-		x = reconb.do_load("recon/domains-hosts/brute_hosts")
-		if bruteforce:
-			x.do_set("WORDLIST " + bruteforce)
-		else:
-			x.do_set("WORDLIST /usr/share/recon-ng/data/hostnames.txt")
-		x.do_set("SOURCE " + domain)
-		x.do_run(None)
-	
-	#reporting output
-	outFile = "FILENAME "+os.getcwd()+"/"+domains[0]
-	x = reconb.do_load("reporting/csv")
-	x.do_set(outFile+".csv")
-	x.do_run(None)
+    wspace = domains[0]
 
-	x = reconb.do_load("reporting/list")
-	x.do_set(outFile+".lst")
-	x.do_set("COLUMN host")
-	x.do_run(None)
+    reconb = base.Recon(base.Mode.CLI)
+    reconb.init_workspace(wspace)
+    reconb.onecmd("TIMEOUT=100")
+    #dns_resolver_ip = get_random_resolver_ip()
+    #reconb.options["nameserver"] = dns_resolver_ip
+    #reconb.onecmd("NAMESERVER={}".format(dns_resolver_ip))
+    module_list = ["recon/domains-hosts/bing_domain_web",
+                   "recon/domains-hosts/shodan_hostname",
+                   "recon/companies-multi/github_miner",
+				   "recon/domains-hosts/google_site_web",
+				   "recon/domains-hosts/netcraft",
+                   "recon/domains-hosts/shodan_hostname",
+                   "recon/domains-hosts/threatcrowd",
+                   "recon/domains-hosts/hackertarget",
+                   "recon/domains-hosts/builtwith",
+                   "recon/domains-hosts/mx_spf_ip",
+                   "recon/netblocks-hosts/shodan_net",
+                   "recon/repositories-vulnerabilities/github_dorks",
+                   "recon/domains-hosts/google_site_api",
+                   "recon/netblocks-companies/whois_orgs",
+                   "recon/domains-vulnerabilities/punkspider",
+                   "recon/domains-vulnerabilities/xssed",
+                   "recon/domains-vulnerabilities/xssposed",
+                   "recon/domains-vulnerabilities/ghdb",
+                   "recon/companies-multi/github_miner",
+                   "recon/hosts-hosts/reverse_resolve",
+                   "recon/repositories-vulnerabilities/github_dorks",
+                   "recon/hosts-hosts/resolve"
+
+                   ]
+
+    for domain in domains:
+        for module in module_list:
+            print("Attempting Module {}".format(module))
+            run_module(reconb, module, domain)
+
+        #subdomain bruteforcing
+        x = reconb.do_load("recon/domains-hosts/brute_hosts")
+        if bruteforce:
+            x.do_set("WORDLIST " + bruteforce)
+        else:
+            x.do_set("WORDLIST /usr/share/recon-ng/data/hostnames.txt")
+            x.do_set("SOURCE " + domain)
+            x.do_run(None)
+
+    outFile = "FILENAME " + os.getcwd() + "/" + domains[0]
+    x = reconb.do_load("reporting/list")
+    x.do_set(outFile+".lst")
+    x.do_set("COLUMN host")
+    x.do_run(None)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', dest='runAltDns', action='store_true', help="After recon, run AltDNS? (this requires alt-dns)")
@@ -80,29 +123,30 @@ parser.add_argument("-p", dest="permlist", type=argparse.FileType('r'), help="in
 args = parser.parse_args()
 
 if args.runAltDns and not altDnsPath:
-	print "Error: no altDns path specified, please download from: https://github.com/infosec-au/altdns"
-	exit(0)
+    print "Error: no altDns path specified, please download from: https://github.com/infosec-au/altdns"
+    exit(0)
 
 domainList = []
 
 if args.domains:
-	domainList+=args.domains
+    domainList+=args.domains
+
 
 if args.filename:
-	lines = args.filename.readlines()
-	lines = [line.rstrip('\n') for line in lines]
-	domainList+=lines
+    lines = args.filename.readlines()
+    lines = [line.rstrip('\n') for line in lines]
+    domainList+=lines
 
 bruteforceList = args.wordlist.name if args.wordlist else ""	
 
 run_recon(domainList, bruteforceList)
 
 if args.runAltDns:
-	workspace = domainList[0]
-	altCmd="python "+os.path.join(altDnsPath,"altdns.py")
-	subdomains = os.path.join(os.getcwd(), workspace+".lst")
-	permList = args.permlist.name if args.permlist else os.path.join(altDnsPath,"words.txt")
-	output = os.path.join(os.getcwd(),workspace+"_output.txt")
-	print "running alt-dns... please be patient :) results will be displayed in "+output
-	# python altdns.py -i subdomainsList -o data_output -w permutationsList -r -s results_output.txt
-	os.system('%s -i %s -o data_output -w %s -r -s %s' % (altCmd, subdomains, permList,output))
+    workspace = domainList[0]
+    altCmd="python "+os.path.join(altDnsPath,"altdns.py")
+    subdomains = os.path.join(os.getcwd(), workspace+".lst")
+    permList = args.permlist.name if args.permlist else os.path.join(altDnsPath,"words.txt")
+    output = os.path.join(os.getcwd(),workspace+"_output.txt")
+    print "running alt-dns... please be patient :) results will be displayed in "+output
+    # python altdns.py -i subdomainsList -o data_output -w permutationsList -r -s results_output.txt
+    os.system('%s -i %s -o data_output -w %s -r -s %s' % (altCmd, subdomains, permList,output))
